df3f2a7a272ec80a16dfc17c6ba7131b387ce956
sama555 <samaromani@gmail.com>
2022-03-28 16:59:09 -0300

    Initial commit

---
+++ src/com/tipos/TipoDatos.java
@@ -0,0 +1,23 @@
+package com.tipos;
+
+
+public class TipoDatos {
+
+    public static void main(String[] args) {
+        int num1=5;
+        double num2=10.05;
+        long num3=8;
+        char caracter='c';
+        boolean verdero=true;
+        boolean falso=false;
+        String cad="hola mundo";
+        System.out.println("Un numero Entero "+num1);
+        System.out.println("Un numero Double "+ num2);
+        System.out.println("Un numero lang "+ num3);
+        System.out.println("Una cadena de caracter: "+ cad);
+        System.out.println("Un caracter: "+ caracter);
+        System.out.println("Un booleano: "+verdero+", "+falso);
+
+    }
+
+}
